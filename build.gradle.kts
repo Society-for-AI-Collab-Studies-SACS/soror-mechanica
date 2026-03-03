c3268283f377bc440a6989f8eb5c4552ef41410a
Justin <justin@aicoherence.org>
2026-03-02 23:18:59 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,16 @@ plugins {
   id("com.google.devtools.ksp") version "2.0.20-1.0.25" apply false
   id("org.jetbrains.kotlin.plugin.compose") version "2.0.20"
 }
+
+buildscript {
+    dependencies {
+        classpath("org.jetbrains.kotlin.plugin.compose:org.jetbrains.kotlin.plugin.compose.gradle.plugin:2.3.10")
+    }
+}
+
+android { … }
+
+composeCompiler {
+    reportsDestination = layout.buildDirectory.dir("compose_compiler")
+    stabilityConfigurationFile = rootProject.layout.projectDirectory.file("stability_config.conf")
+}
